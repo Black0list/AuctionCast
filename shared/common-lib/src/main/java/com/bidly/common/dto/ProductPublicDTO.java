b65050d9b55f658254d03ef782833699c9116686
Black0list <abdelkebir123123@gmail.com>
2026-03-25 11:39:37 +0100

    fix: implement product status lifecycle and synchronization

--- shared/common-lib/src/main/java/com/bidly/common/dto/ProductPublicDTO.java
+++ shared/common-lib/src/main/java/com/bidly/common/dto/ProductPublicDTO.java
@@ -1,16 +1,24 @@
 package com.bidly.common.dto;
 
+import com.bidly.common.enums.ProductStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ProductPublicDTO implements Serializable {
     private UUID id;
     private String title;
     private String description;
+    private String coverImage;
+    private List<ProductImagePublicDTO> imageUrls;
+    private ProductStatus status;
 }
+
